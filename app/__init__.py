8106e1a135efe3486c7fab9851a2bf0a8ad5895d
yntonfon <ynguieye@gmail.com>
2018-04-10 21:27:56 +0200

    feat(*): add default settings file

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,11 @@ from flask import Flask
 
 def create_app(config=None):
     app = Flask(__name__)
+
+    app.config.from_pyfile('default_settings.cfg')
+
+    if config:
+        app.config.from_object(config)
     
     return app
 
