4d2edd2cecf2d7057be6f237d4337123f50ac0bf
yntonfon <ynguieye@gmail.com>
2018-05-07 20:05:35 +0200

    feat(*): add new application exception

---
+++ application/core/exception/dashboardplus_exception.py
@@ -0,0 +1,38 @@
+import abc
+
+
+class DashboardPlusException(Exception, metaclass=abc.ABCMeta):
+    def __init__(self):
+        raise NotImplementedError('users must inherate to use this base class')
+
+
+class OutputException(DashboardPlusException):
+    def __init__(self, messages=None):
+        self.messages = messages
+
+
+class AppDataValidationException(OutputException):
+    pass
+
+
+class AppDataDuplicationException(OutputException):
+    pass
+
+
+class AppUnexpectedFailureException(OutputException):
+    pass
+
+
+class InputException(DashboardPlusException):
+    def __init__(self, messages=None, origins=None):
+        self.origins = origins
+        self.messages = messages
+
+
+class EntityAlreadyExistsException(InputException):
+    def __init__(self, name: str, keys: tuple):
+        self.messages = 'An entity {0} is already created with fields {1}'.format(name, repr(keys))
+
+
+class PersitenceException(InputException):
+    pass
