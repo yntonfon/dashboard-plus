e8397344b81a273fd8347d5904cbcb283bd73298
yntonfon <ynguieye@gmail.com>
2018-05-08 01:13:42 +0200

    feat(*): add test for password security provider

--- application/providers/security/password_security_provider.py
+++ application/providers/security/password_security_provider.py
@@ -1,11 +1,11 @@
-import bcrypt
-
 from application.core.usecase.encrypt_password_port import EncryptPasswordPort
 
 ENCRYPTION_LOG_ROUNDS = 12
 
 
 class PasswordSecurityProvider(EncryptPasswordPort):
+    def __init__(self, crypto):
+        self.crypto = crypto
 
     def encrypt_password(self, password: str):
-        return bcrypt.hashpw(password.encode(), bcrypt.gensalt(ENCRYPTION_LOG_ROUNDS))
+        return self.crypto.hashpw(password.encode(), self.crypto.gensalt(ENCRYPTION_LOG_ROUNDS))
