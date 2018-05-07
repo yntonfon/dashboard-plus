5b53425a616f93695b290078292b42c59b81bb7f
yntonfon <ynguieye@gmail.com>
2018-05-08 01:01:07 +0200

    feat(*): add test for schema and validator

---
+++ tests/unit/providers/validator/test_account_validator_provider.py
@@ -0,0 +1,17 @@
+from unittest import mock
+
+from application.providers.validator.account_validator_provider import AccountValidatorProvider
+from application.providers.validator.schema.account_schema import AccountSchema
+
+
+class TestAccountValidatorProvider:
+    def test_validate_payload_should_call_validate(self):
+        # Given
+        mock_schema = mock.create_autospec(AccountSchema)
+        validator = AccountValidatorProvider(mock_schema)
+
+        # When
+        validator.validate_payload({})
+
+        # Then
+        mock_schema.validate.assert_called_with({})
