cbbbe38544f59a44ac82e70ddf5ef27d1d9be550
yntonfon <ynguieye@gmail.com>
2018-05-07 19:10:10 +0200

    feat(*): add fields to account entity - hash_password - email_confirmed

--- application/core/entity/account.py
+++ application/core/entity/account.py
@@ -1,5 +1,6 @@
 class Account:
-    def __init__(self, username, email, password):
-        self.password = password
+    def __init__(self, username, email, hash_password, email_confirmed):
         self.email = email
         self.username = username
+        self.hash_password = hash_password
+        self.email_confirmed = email_confirmed
