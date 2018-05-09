6b99154084217a3e68f23f299b908b6d9d3fc7bc
yntonfon <ynguieye@gmail.com>
2018-05-09 13:33:41 +0200

    feat(*): remove hashpw work factor

--- tests/unit/providers/security/test_password_security_provider.py
+++ tests/unit/providers/security/test_password_security_provider.py
@@ -16,14 +16,3 @@ class TestPasswordSecurityProvider(UnitTest):
 
         # Then
         mock_bcrypt.hashpw.assert_called_with(b'password', 'salt')
-
-    def test_encrypt_password_should_call_gensalt_with_fix_log_rounds(self):
-        # Given
-        mock_crypto = Mock()
-        provider = PasswordSecurityProvider(mock_crypto)
-
-        # When
-        provider.encrypt_password('password')
-
-        # Then
-        mock_crypto.gensalt.assert_called_with(12)
