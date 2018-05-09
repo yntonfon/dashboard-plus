605b02057e64e34c502ce4d2aa75b46e36d45be4
yntonfon <ynguieye@gmail.com>
2018-05-09 14:37:20 +0200

    feat(*): add AccountValidatorProvider to IOC

--- application/configuration/ioc_providers.py
+++ application/configuration/ioc_providers.py
@@ -3,9 +3,14 @@ from dependency_injector import containers, providers
 
 from application.configuration import config
 from application.providers.security import PasswordSecurityProvider
+from application.providers.validator import AccountValidatorProvider
+from application.providers.validator.account_schema import AccountSchema
 
 
 class IOCProviders(containers.DeclarativeContainer):
     """IoC container of providers."""
 
     password_security_provider = providers.Singleton(PasswordSecurityProvider, crypto=bcrypt, config=config)
+
+    account_schema = providers.Singleton(AccountSchema)
+    account_validator_provider = providers.Singleton(AccountValidatorProvider, schema=account_schema)
