da3d1a88d8771c04097d5ac1610d53df9f817347
yntonfon <ynguieye@gmail.com>
2018-05-09 13:50:14 +0200

    feat(*): use kdf to encrypt pwd instead of hashpw

--- application/providers/security/password_security_provider.py
+++ application/providers/security/password_security_provider.py
@@ -2,8 +2,13 @@ from application.core.port.encrypt_password_port import EncryptPasswordPort
 
 
 class PasswordSecurityProvider(EncryptPasswordPort):
-    def __init__(self, crypto):
+    def __init__(self, crypto, config):
+        self.config = config
         self.crypto = crypto
 
-    def encrypt_password(self, password: str):
-        return self.crypto.hashpw(password.encode(), self.crypto.gensalt())
+    def encrypt_password(self, password: str) -> bytes:
+        salt = self.config['SECRET_KEY'].encode()
+        key_bytes = self.config['BCRYPT_DESIRED_KEY_BYTES']
+        rounds = self.config['BCRYPT_ROUNDS']
+
+        return self.crypto.kdf(password=password.encode(), salt=salt, desired_key_bytes=key_bytes, rounds=rounds)
--- tests/unit/providers/security/test_password_security_provider.py
+++ tests/unit/providers/security/test_password_security_provider.py
@@ -5,14 +5,20 @@ from tests.base_tests import UnitTest
 
 
 class TestPasswordSecurityProvider(UnitTest):
-    def test_encrypt_password_should_call_hashpw(self):
+    def test_encrypt_password_should_call_kdf(self):
         # Given
         mock_bcrypt = Mock()
         mock_bcrypt.gensalt.return_value = 'salt'
-        provider = PasswordSecurityProvider(mock_bcrypt)
+        config = {
+            'SECRET_KEY': 'mysecret',
+            'BCRYPT_DESIRED_KEY_BYTES': 32,
+            'BCRYPT_ROUNDS': 12
+        }
+
+        provider = PasswordSecurityProvider(mock_bcrypt, config)
 
         # When
         provider.encrypt_password('password')
 
         # Then
-        mock_bcrypt.hashpw.assert_called_with(b'password', 'salt')
+        mock_bcrypt.kdf.assert_called_with(password=b'password', salt=b'mysecret', desired_key_bytes=32, rounds=12)
