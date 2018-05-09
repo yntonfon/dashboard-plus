655bbc3064ea71477ec7c31b12023c4e80d4f453
yntonfon <ynguieye@gmail.com>
2018-05-09 15:33:15 +0200

    feat(*): add AccountDataProvider to IOC

--- application/configuration/ioc_providers.py
+++ application/configuration/ioc_providers.py
@@ -2,6 +2,8 @@ import bcrypt
 from dependency_injector import containers, providers
 
 from application.configuration import config
+from application.configuration.ioc_database import IOCDatabase
+from application.providers.data.account_data_provider import AccountDataProvider
 from application.providers.security import PasswordSecurityProvider
 from application.providers.validator import AccountValidatorProvider
 from application.providers.validator.account_schema import AccountSchema
@@ -12,5 +14,7 @@ class IOCProviders(containers.DeclarativeContainer):
 
     password_security_provider = providers.Singleton(PasswordSecurityProvider, crypto=bcrypt, config=config)
 
-    account_schema = providers.Singleton(AccountSchema)
+    account_schema = providers.Factory(AccountSchema)
     account_validator_provider = providers.Singleton(AccountValidatorProvider, schema=account_schema)
+
+    account_database_data_provider = providers.Singleton(AccountDataProvider, session=IOCDatabase.db().session)
