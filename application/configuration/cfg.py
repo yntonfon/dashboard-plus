346dce3dc1818c8693e168e050118dd110175a09
yntonfon <ynguieye@gmail.com>
2018-05-09 14:30:07 +0200

    feat(*): add getter for env vars

--- application/configuration/cfg.py
+++ application/configuration/cfg.py
@@ -1,7 +1,16 @@
 from os import environ
 
+
+def get_int(var, default):
+    return int(environ.get(var, default))
+
+
+def get_bool(var, default):
+    return bool(environ.get(var, default))
+
+
 default_config = {
     'SECRET_KEY': environ.get('SECRET_KEY', 'secret_for_test'),
-    'BCRYPT_DESIRED_KEY_BYTES': environ.get('BCRYPT_DESIRED_KEY_BYTES', 32),
-    'BCRYPT_ROUNDS': 100
+    'BCRYPT_DESIRED_KEY_BYTES': get_int('BCRYPT_DESIRED_KEY_BYTES', 32),
+    'BCRYPT_ROUNDS': get_int('BCRYPT_DESIRED_KEY_BYTES', 100),
 }
