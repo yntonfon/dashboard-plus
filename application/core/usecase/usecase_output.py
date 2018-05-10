aa5bc0639d9783b0c0352dd15b2e20e3784461e4
yntonfon <ynguieye@gmail.com>
2018-05-11 00:09:17 +0200

    feat(*): add new message and description items

--- application/core/usecase/usecase_output.py
+++ application/core/usecase/usecase_output.py
@@ -9,12 +9,16 @@ class UsecaseStatusEnum(Enum):
 class UseCaseMessageEnum(Enum):
     account_not_registered = 'Account registering failed'
     account_registered = 'Account registered with success'
+    account_activated = 'Account activated with success'
+    account_not_activated = 'Account not activated'
 
 
 class UseCaseDescriptionEnum(Enum):
     invalid_input_data = 'Invalid input data'
     account_already_exists = 'Account already exists'
     unexpected_error = 'An unexpected error occured, please try later'
+    invalid_token = 'Token is invalid'
+    account_does_not_exist = 'Account does not exist'
 
 
 class UseCaseOutput:
