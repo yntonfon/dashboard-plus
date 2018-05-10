b974634678bdefda46177898226f1be4693abd51
yntonfon <ynguieye@gmail.com>
2018-05-10 13:04:50 +0200

    feat(*): add usecase input/output class

---
+++ application/core/usecase/usecase_input.py
@@ -0,0 +1,3 @@
+class UsecaseInput:
+    def __init__(self, payload=None):
+        self.payload = payload or {}
---
+++ application/core/usecase/usecase_output.py
@@ -0,0 +1,6 @@
+class UsecaseOutput:
+    def __init__(self, status, message, description, content):
+        self.status = status
+        self.message = message
+        self.description = description
+        self.content = content
