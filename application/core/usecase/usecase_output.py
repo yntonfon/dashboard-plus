0edffdc1a74f1f62e2cd5c7c8c00a3e58e875c32
yntonfon <ynguieye@gmail.com>
2018-05-10 14:14:13 +0200

    feat(*): add UsecaseStatusEnum and UsecaseMessageEnum

--- application/core/usecase/usecase_output.py
+++ application/core/usecase/usecase_output.py
@@ -1,5 +1,19 @@
+from enum import Enum
+
+
+class UsecaseStatusEnum(Enum):
+    success = 'SUCCESS'
+    failure = 'FAILURE'
+
+
+class UsecaseMessageEnum(Enum):
+    invalid_data = 'Invalid data'
+    account_already_exist = 'Account already exists'
+    unexpected_error = 'An unexpected error occured, please try later'
+
+
 class UsecaseOutput:
-    def __init__(self, status, message, description, content):
+    def __init__(self, status=None, message=None, description=None, content=None):
         self.status = status
         self.message = message
         self.description = description
