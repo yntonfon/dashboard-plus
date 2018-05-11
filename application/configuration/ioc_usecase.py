a8807428982d7f7a45326dd6af3f0a886513b719
yntonfon <ynguieye@gmail.com>
2018-05-11 03:13:04 +0200

    feat(*): add activate_account_use_case to IOC

--- application/configuration/ioc_usecase.py
+++ application/configuration/ioc_usecase.py
@@ -2,7 +2,7 @@ from dependency_injector import containers, providers
 
 from application.configuration.ioc_providers import IOCProviders
 from application.core.factory.account_factory import AccountFactory
-from application.core.usecase import RegisterAccountUseCase
+from application.core.usecase import RegisterAccountUseCase, ActivateAccountUseCase
 from application.core.usecase.steps import CreateAccountActivationTokenStep
 from application.core.usecase.steps.create_account_step import CreateAccountStep
 
@@ -31,4 +31,11 @@ class IOCUseCase(containers.DeclarativeContainer):
         RegisterAccountUseCase,
         create_account_step=IOCUseCaseSteps.create_account_step,
         create_token_step=IOCUseCaseSteps.create_account_activation_token_step
+    )
+
+    activate_account_use_case = providers.Factory(
+        ActivateAccountUseCase,
+        decode_token_provider=IOCProviders.token_security_provider,
+        does_account_exist_provider=IOCProviders.account_database_data_provider,
+        update_email_confirmed_provider=IOCProviders.account_database_data_provider
     )
